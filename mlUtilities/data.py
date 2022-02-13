8b697d51e56aeccb32d6f93540bbcf7209f07552
1sandovalgerardo <1sandoval.gerardo@gmail.com>
2022-02-13 11:52:23 -0600

    Added command line ability for script
    You can pass your ticker through the command line now.
    data for vfh vgt qcom collect.

--- mlUtilities/data.py
+++ mlUtilities/data.py
@@ -3,9 +3,26 @@
 import pandas as pd
 import pandas_datareader.data as web
 import yfinance as yf
+import argparse
+import traceback
 
 INTERVAL_SYMBOLS = ['1m', '2m', '5m', '15m', '30m', '60m', '90m', 
                     '1h', '1d', '5d', '1wk', '1mo', '3mo'] 
+
+
+def Parser():
+    """Create Parser.
+    returns: parser.parsearguments() object"""
+    program_description = 'run as: python get_data.py <ticker symbol>'
+    parser = argparse.ArgumentParser(description='Download Stock OHLCV')
+    parser.add_argument("TICKER",
+                        metavar = "<TickerSymbol>",
+                        type=str,
+                        help = 'Enter ticker you wish to get data for')
+    args = parser.parse_args()
+    return args
+
+
 def get_daily_data(ticker, start_date=None):
     ticker = ticker.upper()
     if start_date:
@@ -29,10 +46,18 @@ def get_intraday_data(ticker, interval):
 
 
 def main():
-    daily_data = get_daily_data('spy')
-    intra_data = get_intraday_data('spy', '5m')
-    daily_data.to_csv('1d_SPY')
-    intra_data.to_csv('5m_SPY')
+    parser = Parser()
+    try:
+        if parser:
+            ticker = parser.TICKER.upper()
+            print('Getting data for ', ticker)
+            daily_data = get_daily_data(ticker)
+            intra_data = get_intraday_data(ticker, '5m')
+            daily_data.to_csv(f'{ticker}_1d_SPY.csv')
+            intra_data.to_csv(f'{ticker}_5m_SPY.csv')
+        return None
+    except Exception:
+        print(traceback.format_exc())
 
 if __name__=='__main__':
     main()
